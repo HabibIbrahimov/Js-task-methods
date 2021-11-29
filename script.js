7e938bd7cf28c0352cb3e70db7972b1fb8248683
HabibIbrahimov <habibyi@code.edu.az>
2021-11-29 23:35:04 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,66 @@
+
+//------Task1--------
+// var str = 'Welcome to JavaScript'
+
+// function reverseByWord(s) {
+//     return s.split("").reverse().join("");
+// }
+// console.log(reverseByWord(str));
+
+//---------Task2---------
+// let arr = [3, 4, 5, 6];
+//map----------
+
+// for (let i = 0; i < arr.length; i++) {
+//     arr[i] = arr[i] * 3;
+// }
+// console.log(arr);
+
+//reduce---------
+
+// let sum=0;
+// for (let index = 0; index < array.length; index++) {
+//     sum+=arr[index];
+// }
+// console.log(sum);
+
+//Find------------
+// for (let index = 0; index < array.length; index++) {
+//     if(arr[index]>3){
+//         console.log(arr[index]);
+//         break;
+//     }
+    
+// }
+
+//Some------------
+// var some=false;
+// for (let index = 0; index < arr.length; index++) {
+//     if(arr[index] > 5){
+//             some=true;
+//             break;
+//         }   
+//     }
+// console.log(some);
+
+//Filter-----------------
+// for (let index = 0; index < array.length; index++) {
+//     if(arr[index]>3){
+//         console.log(arr[index]);
+//     }
+    
+// }
+
+//Every-------------
+// for (let index = 0; index < array.length; index++) {
+//     if(arr[index]>3){
+//         console.log(true);
+//     }
+//     else{
+//     console.log(false);
+//     break;
+//     }    
+// }
+
+
+
